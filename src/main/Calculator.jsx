6079b1a867253c942a4f9f8988954846ddf9dc8c
Donizete <felipedonizetebarbosa@gmail.com>
2022-02-07 14:04:12 -0300

    add backspace function

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -116,17 +116,22 @@ export default class Calculator extends Component {
             values[0] = result
             values[1] = 0
 
+
+            if (isNaN(values[0]) || !isFinite(values[0])) {         
+                this.clearMemory()
+            return
+            }
+
+
             const strResult = result.toString();
             this.setFont(strResult)
             
             
-
             this.setState({
                 values,
                 currentValue: result,
                 i: 1
             })
-
         }else{
             i = 1
         }
